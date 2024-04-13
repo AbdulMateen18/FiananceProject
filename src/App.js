e4e2cdd12d534ef76d5f4131f448f8623a3300b9
Abdul Mateen <121253565+AbdulMateen18@users.noreply.github.com>
2024-04-13 22:39:03 +0500

    added assets folder

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import Navbar from "./components/Navbar.jsx";
 import Hero from "./components/Hero.jsx";
+import Analytics from "./components/Analytics.jsx";
 function App() {
   return (
     <div>
       <Navbar />
       <Hero />
+      <Analytics />
     </div>
   );
 }
